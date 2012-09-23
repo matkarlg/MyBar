765115c183bebee99745e0b5b4277eb0c3511356
Viktor Edsand <viktoredsand@gmail.com>
2012-09-23 14:10:35 +0200

    Deleted our first test-application, added a new one more suited for the handin02

--- gen/se/turbotorsk/mybar/R.java
+++ gen/se/turbotorsk/mybar/R.java
@@ -15,14 +15,17 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070003;
+        public static final int GridLayout1=0x7f070002;
+        public static final int listView1=0x7f070003;
+        public static final int menu_settings=0x7f070004;
         public static final int pager=0x7f070000;
         public static final int pager_title_strip=0x7f070001;
-        public static final int ratingBar1=0x7f070002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int testactivity=0x7f030001;
+        public static final int main=0x7f030000;
+        public static final int menu_mybar=0x7f030001;
+        public static final int menu_myfavourites=0x7f030002;
+        public static final int menu_toprated=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
