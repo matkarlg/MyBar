ca9416e8464fa74e3e2ab14d4e08c293513857df
Mathias Karlgren <mathias.karlgren@gmail.com>
2012-09-26 02:05:15 +0200

    Added rating, type of glass, ... columns. Commented out Date since we don't know if we're going to use it for timestamps.

--- src/se/turbotorsk/mybar/Drink.java
+++ src/se/turbotorsk/mybar/Drink.java
@@ -1,120 +1,96 @@
-/**
- * 
- */
 package se.turbotorsk.mybar;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.text.ParseException;
-import android.util.Log;
+//import java.text.SimpleDateFormat;
+//import java.util.Calendar;
+//import java.util.Date;
+//import java.text.ParseException;
+//import android.util.Log;
 
 /**
  * Drink class
  * 
- * @author karlgren
- *
+ * @author Karlgren
+ * 
  */
 public class Drink {
-	private int ID = 0;
+	private int _id = 0;
 	private String name = "";
+	private String glass = "";
 	private String ingredient = "";
+	private String description = "";
 	private int rating = 0;
 	// IMG blob;
-	
-	Calendar calendar = Calendar.getInstance();
+
+	/*private Calendar calendar = Calendar.getInstance();
 	private Date date = new Date(calendar.getTime().getTime());
 	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	
-	/**
-     * 
-     * @return returns the date of the event as a string. 
-     */
-    public String getDate() {
+
+	public String getDate() {
 		return dateFormat.format(date);
 	}
-    
-    /**
-     * This method is used to set the date when the event was bought.
-     * 
-     * @param year should be an Integer
-     * @param month should be an Integer
-     * @param day should be an Integer
-     */
-    public void setDate(int year, int month, int day) {
+
+	public void setDate(int year, int month, int day) {
 		try {
 			this.date = dateFormat.parse("" + year + "-" + month + "-" + day);
 		} catch (ParseException e) {
-			Log.e(e.getClass().getName(), "ParseException: " + e.getMessage(), e);
+			Log.e(e.getClass().getName(), "ParseException: " + e.getMessage(),
+					e);
 		}
 	}
-    
-    /**
-     * The in-parameter of this class should be an Date object.
-     * 
-     * @param date should be an Date object.
-     */
-    public void setDate(Date date) {
+
+	public void setDate(Date date) {
 		try {
 			this.date = dateFormat.parse(date.toString());
 		} catch (ParseException e) {
-			Log.e(e.getClass().getName(), "ParseException: " + e.getMessage(), e);
+			Log.e(e.getClass().getName(), "ParseException: " + e.getMessage(),
+					e);
 		}
+	}*/
+	
+	public int get_id() {
+		return _id;
 	}
 
-	/**
-	 * @return the iD
-	 */
-	public int getID() {
-		return ID;
+	public void set_id(int _id) {
+		this._id = _id;
 	}
-
-	/**
-	 * @param iD the iD to set
-	 */
-	public void setID(int iD) {
-		ID = iD;
-	}
-
-	/**
-	 * @return the name
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the ingredient
-	 */
 	public String getIngredient() {
 		return ingredient;
 	}
 
-	/**
-	 * @param ingredient the ingredient to set
-	 */
 	public void setIngredient(String ingredient) {
 		this.ingredient = ingredient;
 	}
 
-	/**
-	 * @return the rating
-	 */
 	public int getRating() {
 		return rating;
 	}
 
-	/**
-	 * @param rating the rating to set
-	 */
 	public void setRating(int rating) {
 		this.rating = rating;
+	}
+
+	public String getGlass() {
+		return glass;
+	}
+
+	public void setGlass(String glass) {
+		this.glass = glass;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 }
