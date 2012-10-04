0f973accb5daab8ed0881f0e83b849a28930c3cb
Bjurbäck <mathias.bjurback@gmail.com>
2012-10-04 15:57:08 +0200

    Tried to add onclick item to mybar view

--- src/se/turbotorsk/mybar/Add_Ingredient_Activity.java
+++ src/se/turbotorsk/mybar/Add_Ingredient_Activity.java
@@ -1,30 +1,31 @@
 package se.turbotorsk.mybar;
 
 import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Add_Ingredient_Activity extends ListActivity {
+	
+	TextView dName;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dName = (TextView) GridLayout1.getContentView().findViewById(R.id.textView1);
         String[] ingredients = this.getResources().getStringArray(R.array.ingredient_list);
 	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 	android.R.layout.simple_list_item_1, ingredients);
-	    setListAdapter(adapter);  
+	    setListAdapter(adapter);
         }
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 	  String item = (String) getListAdapter().getItem(position);
-	  Intent data = new Intent();
-	  data.putExtra("Add",item);
+	  dName.setText(item);
 	  Toast.makeText(this, "Added ingredient", Toast.LENGTH_LONG).show();
-	  setResult(RESULT_OK, data);
 	  finish();
 	}
 	
