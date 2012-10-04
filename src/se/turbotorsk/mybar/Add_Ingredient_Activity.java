3cf188587c3858204ef16c9944fbef79f72de8a4
Bjurbäck <mathias.bjurback@gmail.com>
2012-10-04 16:02:04 +0200

    Reverted back

--- src/se/turbotorsk/mybar/Add_Ingredient_Activity.java
+++ src/se/turbotorsk/mybar/Add_Ingredient_Activity.java
@@ -5,17 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class Add_Ingredient_Activity extends ListActivity {
 	
-	TextView dName;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        dName = (TextView) GridLayout1.getContentView().findViewById(R.id.textView1);
         String[] ingredients = this.getResources().getStringArray(R.array.ingredient_list);
 	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 	android.R.layout.simple_list_item_1, ingredients);
 	    setListAdapter(adapter);
@@ -24,8 +21,7 @@ public class Add_Ingredient_Activity extends ListActivity {
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 	  String item = (String) getListAdapter().getItem(position);
-	  dName.setText(item);
-	  Toast.makeText(this, "Added ingredient", Toast.LENGTH_LONG).show();
+	  Toast.makeText(this, "Added " + item, Toast.LENGTH_LONG).show();
 	  finish();
 	}
 	
