30daff1afbc0420c3010d31e220fb234f047e73b
Viktor Edsand <viktoredsand@gmail.com>
2012-10-04 15:00:43 +0200

    Added the options-menu without the Add-button in Settings-activity

--- src/se/turbotorsk/mybar/Settings_Activity.java
+++ src/se/turbotorsk/mybar/Settings_Activity.java
@@ -1,7 +1,10 @@
 package se.turbotorsk.mybar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.app.Activity;
+import android.content.Intent;
 
 public class Settings_Activity extends Activity {
 
@@ -9,6 +12,25 @@ public class Settings_Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_settings);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_about_settings, menu);
+        return true;
+    }
+    
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+        	case R.id.menu_settings:
+        		startActivity(new Intent(this, Settings_Activity.class));
+        		return true;
+        	case R.id.menu_about:
+        		AboutBox.Show(Settings_Activity.this);
+        		return true;
+        	default:
+        		return true;
+    	}
     }
 
 }
