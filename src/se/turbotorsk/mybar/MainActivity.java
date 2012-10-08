97f0a495ba7194bcef9c48b03f43ef3bc6570071
Mathias Karlgren <mathias.karlgren@gmail.com>
2012-10-08 12:55:37 +0200

    Fixed spelling mistake

--- src/se/turbotorsk/mybar/MainActivity.java
+++ src/se/turbotorsk/mybar/MainActivity.java
@@ -3,40 +3,30 @@ New BSD License
 Copyright (c) 2012, MyBar Team All rights reserved.
 mybar@turbotorsk.se
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-•	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-•	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-•	Neither the name of the MyBar nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ï¿½	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ï¿½	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ï¿½	Neither the name of the MyBar nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 
 package se.turbotorsk.mybar;
 
-import android.app.TabActivity;
-//import android.app.ActionBar;
-//import android.app.FragmentTransaction;
 import se.turbotorsk.mybar.model.Data;
-import android.content.Context;
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
-
-import android.os.Parcelable;
-//import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-//import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-//import android.support.v4.app.NavUtils;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
-//import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+//import android.app.ActionBar;
+//import android.app.FragmentTransaction;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.NavUtils;
+//import android.view.Gravity;
 //import android.view.MenuItem;
-import android.view.View;
 //import android.view.ViewGroup;
 //import android.widget.TextView;
 
@@ -72,7 +62,7 @@ public class MainActivity extends TabActivity {
 		Intent intentCollection = new Intent().setClass(this, CollectionActivity.class);
 		TabSpec tabSpecCollection = tabHost
 			.newTabSpec("Collection")
-			.setIndicator("Colleciton")
+			.setIndicator("Collection")
 			.setContent(intentCollection);
 	
 		// add all tabs 
