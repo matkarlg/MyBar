c5dcb4bbb0f3835ae6eb6e49626075a958bfc6b1
Bjurbäck <mathias.bjurback@gmail.com>
2012-10-10 15:48:28 +0200

    Added a profile button in settings.

---
+++ src/se/turbotorsk/mybar/Profiles.java
@@ -0,0 +1,33 @@
+package se.turbotorsk.mybar;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class Profiles extends ListActivity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String[] profiles = {"Home","Dag's Place", "School"};
+	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 	android.R.layout.simple_list_item_1, profiles);
+	    setListAdapter(adapter);
+        }
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+	  //Bundle bundle = new Bundle();
+	  String item = (String) getListAdapter().getItem(position);
+	  Intent data = new Intent(Profiles.this, Settings_Activity.class);
+	 // bundle.putString("profile",item);
+	  data.putExtra("profile",item);
+      setResult(RESULT_OK, data);
+	  Toast.makeText(this, "Changed to " + item, Toast.LENGTH_LONG).show();
+	  finish();
+	}
+
+}
--- src/se/turbotorsk/mybar/Settings_Activity.java
+++ src/se/turbotorsk/mybar/Settings_Activity.java
@@ -3,27 +3,45 @@ New BSD License
 Copyright (c) 2012, MyBar Team All rights reserved.
 mybar@turbotorsk.se
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-•	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-•	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-•	Neither the name of the MyBar nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ï¿½	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ï¿½	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ï¿½	Neither the name of the MyBar nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 
 package se.turbotorsk.mybar;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.app.Activity;
-import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Settings_Activity extends Activity {
-
+	Button profileButton;
+	TextView profile1;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_settings);
+        setContentView(R.layout.view_settings); 
+        profile1 = (TextView) findViewById(R.id.textView1);
+        profileButton = (Button) findViewById(R.id.button1);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent1 = new Intent(Settings_Activity.this, Profiles.class);
+				startActivity(intent1);
+				Intent i = getIntent();
+				Bundle bundle = i.getExtras();
+				//String profile = bundle.getString("profile");
+				profile1.setText("Dag");
+			}
+        });  
     }
     
     @Override
@@ -31,6 +49,7 @@ public class Settings_Activity extends Activity {
         getMenuInflater().inflate(R.menu.activity_about_settings, menu);
         return true;
     }
+  
     
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()) {
