f8dd1ce52a8c4e188f15bdb26263280f99e22f8d
Viktor Edsand <viktoredsand@gmail.com>
2012-10-10 15:20:28 +0200

    Updated the comments and some strings in Share

--- src/se/turbotorsk/mybar/Share.java
+++ src/se/turbotorsk/mybar/Share.java
@@ -29,12 +29,12 @@ public class Share extends Activity {
     }
 
     /**
-     * The method that is used to send the information that will be sended
+     * The method that is used to send the information that will be sent
      */
     private void share() {
     	Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
     	sharingIntent.setType("text/plain");
-    	String shareBody = "Here is the share content body";
+    	String shareBody = "Insert the message to send here";
     	sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
     	sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
     	startActivity(Intent.createChooser(sharingIntent, "Share via"));
