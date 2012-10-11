60acb6f696f0ba25f869526b55f5ab889db5431b
Mathias Karlgren <mathias.karlgren@gmail.com>
2012-10-11 15:55:54 +0200

    Added the new table ingredient beyond the Drink table to MyBarDatabaseHelper

--- src/se/turbotorsk/mybar/model/database/MyBarDatabaseHelper.java
+++ src/se/turbotorsk/mybar/model/database/MyBarDatabaseHelper.java
@@ -3,12 +3,11 @@ New BSD License
 Copyright (c) 2012, MyBar Team All rights reserved.
 mybar@turbotorsk.se
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-•	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-•	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-•	Neither the name of the MyBar nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ï¿½	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ï¿½	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ï¿½	Neither the name of the MyBar nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
+ */
 
 package se.turbotorsk.mybar.model.database;
 
@@ -18,11 +17,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 /**
- * SQLite helper object to manage and create our database. Uses DrinkTable.java
- * (and in the future maybe more) as a template for every table that should be
- * in the database. Uses onUpgrade() to update the database if a newer version
- * of the database is out. (a software update may change the DATABASE_VERSION to
- * a higher number calling onUpgrade()).
+ * SQLite helper object to manage and create our database. Uses *Table.java as a
+ * template for every table that should be in the database. Uses onUpgrade() to
+ * update the database if a newer version of the database is out. (a software
+ * update may change the DATABASE_VERSION to a higher number calling
+ * onUpgrade()).
  * 
  * @author Mathias Karlgren (matkarlg)
  * 
@@ -40,6 +39,7 @@ public class MyBarDatabaseHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase database) {
 		// Populate the database using our table classes
 		DrinkTable.onCreate(database);
+		IngredientsTable.onCreate(database);
 	}
 
 	// Method is called during an upgrade of the database
@@ -47,7 +47,9 @@ public class MyBarDatabaseHelper extends SQLiteOpenHelper {
 	public void onUpgrade(SQLiteDatabase database, int oldVersion,
 			int newVersion) {
 		// Upgrade our database with the new table versions
-		Log.w(this.getClass().getName(), "Upgrading database from version " + oldVersion + " to " + newVersion);
+		Log.w(this.getClass().getName(), "Upgrading database from version "
+				+ oldVersion + " to " + newVersion);
 		DrinkTable.onUpgrade(database, oldVersion, newVersion);
+		IngredientsTable.onUpgrade(database, oldVersion, newVersion);
 	}
 }
