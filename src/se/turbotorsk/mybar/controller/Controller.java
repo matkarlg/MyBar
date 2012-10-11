03c396e017f8949dafa918d7dd10b825781d615e
Mathias Karlgren <mathias.karlgren@gmail.com>
2012-10-11 14:35:16 +0200

    Fixed yo getDrinkNamesAsArray(). Added license

--- src/se/turbotorsk/mybar/controller/Controller.java
+++ src/se/turbotorsk/mybar/controller/Controller.java
@@ -1,83 +1,89 @@
+/*
+New BSD License
+Copyright (c) 2012, MyBar Team All rights reserved.
+mybar@turbotorsk.se
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+�	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+�	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+�	Neither the name of the MyBar nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package se.turbotorsk.mybar.controller;
 
 import java.util.LinkedList;
 
-import android.content.ContentResolver;
 import se.turbotorsk.mybar.model.Data;
 import se.turbotorsk.mybar.model.Drink;
+import android.util.Log;
 
 public class Controller {
-	//public final static Controller controller = new Controller();
-	//private CoreLib cl = new CoreLib();
-	private static Data data = null;
+	private final static Data data = new Data();
 	private static int myBarID = 1;
-	private static DrinkManager dm = null;
-	//		ContentResolver contentResolver = CoreLib.ContentResolver();
-	public Controller()
-	{
-		//contentResolver = CoreLib.ContentResolver();
-		dm = new DrinkManager();
-		
-		//data = new Data(contentResolver);	
+	private final static DrinkManager dm = new DrinkManager();
+
+	public static LinkedList<Drink> getMyBar() {
+		// dm.getMyBar(data.getMyIngredients(this.myBarID),
+		// data.getAllDrinks(this.contentResolver)); Implement this in data....
+		return null;
 	}
 
-	public static LinkedList<Drink> getMyBar()
-	{
-		//dm.getMyBar(data.getMyIngredients(this.myBarID), data.getAllDrinks(this.contentResolver));			Implement this in data....
-		return null;
-	}
-	
-	public static String[] getDrinkNamesAsArray()
-	{
-		String[] array = null;
-		int i = 0;
-		for(Drink drink : data.getAllDrinks()){
-			array[i++] = drink.getName();	
+	public static String[] getDrinkNamesAsArray() {
+		LinkedList<String> nameList = new LinkedList<String>();
+
+		for (int i = 0; i < data.getAllDrinks().size(); i++) {
+			nameList.add(data.getAllDrinks().get(i).getName());
+			
+			Log.d(Controller.class.getClass().getName(), ""
+					+ data.getAllDrinks().get(i).get_id() + " "
+					+ data.getAllDrinks().get(i).getName());
 		}
+
+		String[] array = new String[nameList.size()];
+		nameList.toArray(array);
+
 		return array;
 	}
-	
-	
-	public static Drink getIngredientById(int id){
+
+	public static Drink getIngredientById(int id) {
 		return null;
 	}
-	//-----------------------------------------------------Methods for getting and setting favorite drinks ----------------------------------
-	
-	
-	public static boolean addFavoriteDrink(int id){
+
+	// -----------------------------------------------------Methods for getting
+	// and setting favorite drinks ----------------------------------
+
+	public static boolean addFavoriteDrink(int id) {
 		return false;
 	}
-	
-	public static LinkedList<Drink> getFavoritDrinks()
-	{
+
+	public static LinkedList<Drink> getFavoritDrinks() {
 		return null;
 	}
-	
-	// -----------------------------------------------------Methods for Ingredient categories ------------------------------------
-	
-	public static boolean addMyBarCat(String Name)
-	{
-		return false; //data.AddMyBarCat(String name);							Add this method in data. 
+
+	// -----------------------------------------------------Methods for
+	// Ingredient categories ------------------------------------
+
+	public static boolean addMyBarCat(String Name) {
+		return false; // data.AddMyBarCat(String name); Add this method in data.
 	}
-	
-	public static boolean addIngredientToList(int ingredientID)
-	{
-		// data.AddIngredientToList(ingredientID, this.myBarID); 				Add this method in data. 
-		return false; 
+
+	public static boolean addIngredientToList(int ingredientID) {
+		// data.AddIngredientToList(ingredientID, this.myBarID); Add this method
+		// in data.
+		return false;
 	}
-	
-	public static boolean changeMyBarID(int myBarID)
-	{
-		if(true){ //Kolla om ID finns
+
+	public static boolean changeMyBarID(int myBarID) {
+		if (true) { // Kolla om ID finns
 			Controller.myBarID = myBarID;
 			return true;
-		}	
-		else return false; 
+		} else
+			return false;
 	}
-	
-	public static String[][] listMyBarIngredientCategories()
-	{
-		//data.listMyBarIngredientCategories(this.myBarID);						Add this method in data.
+
+	public static String[][] listMyBarIngredientCategories() {
+		// data.listMyBarIngredientCategories(this.myBarID); Add this method in
+		// data.
 		return null;
 	}
 }
