7a1a45e2d51707bbdcdc2eaf0ad89aab74b09afe
Viktor Edsand <viktoredsand@gmail.com>
2012-10-14 12:58:08 +0200

    Added comments for the methods

--- src/se/turbotorsk/mybar/View_Drink_Activity.java
+++ src/se/turbotorsk/mybar/View_Drink_Activity.java
@@ -7,8 +7,7 @@ Redistribution and use in source and binary forms, with or without modification,
 �	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 �	Neither the name of the MyBar nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
+ */
 
 package se.turbotorsk.mybar;
 
@@ -24,11 +23,11 @@ import android.widget.Toast;
 
 /**
  * This activity push the information about a drink to the GUI
- *
+ * 
  */
 
 public class View_Drink_Activity extends Activity {
-	
+
 	TextView dName;
 	TextView dDescription;
 	TextView dIngredients;
@@ -36,80 +35,98 @@ public class View_Drink_Activity extends Activity {
 	ImageView dImage;
 	CheckBox checkBox;
 	String name, rating, description, ingredients;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	
-    
-    	
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_drink);
-        
-        dName = (TextView) findViewById(R.id.drinkName);
-        dDescription = (TextView) findViewById(R.id.drinkDescription);
-        dIngredients = (TextView) findViewById(R.id.drinkIngredients);
-        dRating = (TextView) findViewById(R.id.drinkRating);
-        dImage = (ImageView) findViewById(R.id.drinkImage);
-        checkBox = (CheckBox) findViewById(R.id.drinkFav);
-        
-        setDrinkInfo();
-        checkBoxListener();
-       
-    }
-    
-    /**
-     * This method inserts the information to the Views in this activity's XML-file
-     */
-    public void setDrinkInfo() {
-    	
-    	//Receiving intents from activity
-    	Bundle bundle = getIntent().getExtras();
-    	name = bundle.getString("drinkname");
-    	rating = bundle.getString("rating");
-    	description = bundle.getString("descrip");
-    	ingredients = bundle.getString("ingredients");
-    	
-    	setDrinkName();
-    	setDrinkRating();
-    	setDrinkDescription();
-    	setDrinkIngredients();
-    	setDrinkImage();
-    	
-    }
-    
-    public void setDrinkName() {
-    	dName.setText(name);
-    }
-    
-    public void setDrinkRating() {
-    	dRating.setText(rating);
-    }
-    
-    public void setDrinkDescription() {
-    	dDescription.setText(description);
-    }
-    
-    public void setDrinkIngredients() {
-    	dIngredients.setText(ingredients);
-    }
-    
-    public void setDrinkImage() {
-    	dImage.setImageResource(R.drawable.ic_drinkicon);
-    }
-    
-    public void checkBoxListener() {
-     
-    	checkBox.setOnClickListener(new OnClickListener() {
-     
-    	  public void onClick(View v) {
-                    //is the checkbox checked?
-    		if (((CheckBox) v).isChecked()) {
-    			Data.setFavoriteByName(name);
-    			Toast.makeText(View_Drink_Activity.this,"Added to Favorites", Toast.LENGTH_LONG).show();
-    		}
-    		
-    	  }
-    	});
-    }
-   
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.view_drink);
+
+		dName = (TextView) findViewById(R.id.drinkName);
+		dDescription = (TextView) findViewById(R.id.drinkDescription);
+		dIngredients = (TextView) findViewById(R.id.drinkIngredients);
+		dRating = (TextView) findViewById(R.id.drinkRating);
+		dImage = (ImageView) findViewById(R.id.drinkImage);
+		checkBox = (CheckBox) findViewById(R.id.drinkFav);
+
+		setDrinkInfo();
+		checkBoxListener();
+
+	}
+
+	/**
+	 * This method collects all the set-methods for the information of the drink
+	 */
+	public void setDrinkInfo() {
+
+		// Receiving intents from activity
+		Bundle bundle = getIntent().getExtras();
+		name = bundle.getString("drinkname");
+		rating = bundle.getString("rating");
+		description = bundle.getString("descrip");
+		ingredients = bundle.getString("ingredients");
+
+		setDrinkName();
+		setDrinkRating();
+		setDrinkDescription();
+		setDrinkIngredients();
+		setDrinkImage();
+
+	}
+
+	/**
+	 * This method sets the name of the drink
+	 */
+	public void setDrinkName() {
+		dName.setText(name);
+	}
+
+	/**
+	 * This method sets the rating of the drink
+	 */
+	public void setDrinkRating() {
+		dRating.setText(rating);
+	}
+
+	/**
+	 * This method sets the description of the drink
+	 */
+	public void setDrinkDescription() {
+		dDescription.setText(description);
+	}
+
+	/**
+	 * This method sets the ingredients of the drink
+	 */
+	public void setDrinkIngredients() {
+		dIngredients.setText(ingredients);
+	}
+
+	/**
+	 * This method sets the image of the drink
+	 */
+	public void setDrinkImage() {
+		dImage.setImageResource(R.drawable.ic_drinkicon);
+	}
+
+	/**
+	 * This method handle the checkbox which is used to mark the drink as
+	 * favorite
+	 */
+	public void checkBoxListener() {
+
+		checkBox.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// is the checkbox checked?
+				if (((CheckBox) v).isChecked()) {
+					Data.setFavoriteByName(name);
+					Toast.makeText(View_Drink_Activity.this,
+							"Added to Favorites", Toast.LENGTH_LONG).show();
+				}
+
+			}
+		});
+	}
+
 }
