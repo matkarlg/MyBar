41dd31cb6b17c6a77ed4b2c109ebb3b68e86d29e
Adam Clark <adam.clark91@gmail.com>
2012-10-18 22:00:28 +0200

    Removed violations: MyFavorites

--- src/se/turbotorsk/mybar/MyFavorites.java
+++ src/se/turbotorsk/mybar/MyFavorites.java
@@ -59,11 +59,6 @@ public class MyFavorites extends ListActivity {
 	 */
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		// String item = (String) getListAdapter().getItem(position);
-		// We will replace this to start another activity instead. Dont know how
-		// to do that yet.
-		// Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
-
 		Intent intent = new Intent(this, ViewDrinkActivity.class);
 		intent.putExtra("drinkname", getAdapter().getDrinkName(position));
 		intent.putExtra("rating", getAdapter().getRating(position));
